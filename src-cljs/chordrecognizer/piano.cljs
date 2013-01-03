c976d341107eff1d9b0c20bbac421e6adc1b470c
DanielT <turczanski@gmail.com>
2013-01-03 17:12:19 +0100

    working server side chord recognition

--- src-cljs/chordrecognizer/piano.cljs
+++ src-cljs/chordrecognizer/piano.cljs
@@ -1,12 +1,32 @@
 (ns chordrecognizer.piano
   (:use [domina :only [log attr by-id set-text! classes set-classes!]]
         [domina.css :only [sel]]
-        [domina.events :only [listen! target]]))
-  ;(:require [goog.events :as goog.events])
+        [domina.events :only [listen! target]])
+  (:require 
+    [cljs.reader :as reader]
+    [clojure.string :as string]
+    [goog.net.XhrIo :as xhrio]
+    [goog.net.EventType :as event-type]
+    [goog.events :as events]
+    [goog.structs.Map :as Map]))
+
+  
+  
+(defn request [uri callback method data]
+  (let [req (new goog.net.XhrIo)]
+    (events/listen req goog.net.EventType/COMPLETE callback)
+    (. req (send uri
+                 method
+                 ;; replacing unicode characters until read-string
+                 ;; can deal with \x (which is equivalent to \u00).
+                 ;; See: http://dev.clojure.org/jira/browse/CLJ-1025
+                 (string/replace (pr-str data) #"\\x" "\\u00")
+                 (new goog.structs.Map
+                      "Content-Type" "text/plain; charset=utf-8")))))
 
 (def keys-pressed (atom #{}))
 
-(defn update-chord-guess [chord]
+(defn ^export update-chord-guess [chord]
   (set-text! (by-id "chord") (str "Chord: " chord)))
 
 (defn key-clicked [e]
@@ -22,10 +42,21 @@
         (swap! keys-pressed conj key))
       ))
   (log (map #(str % ", ")  @keys-pressed))
-  (update-chord-guess (map #(str % ", ")  @keys-pressed)))
+  (request "/chord" (fn [e]
+                      (update-chord-guess nil)
+                      (let [xhr (.-target e)
+                            status (. xhr (getStatus))]
+                        (if (= status 200)
+                          (let [chord (reader/read-string
+                                             (. xhr (getResponseText)))]
+                            (update-chord-guess chord))))) 
+           "POST" @keys-pressed))
+  ;(update-chord-guess (map #(str % ", ")  @keys-pressed)))
 
 (defn ^export initPiano[]
   (let [keys (.getElementsByClassName js/document "key")]
     (listen! keys :click key-clicked)))
 
 (defn ^export sell [expr] (sel expr))
+
+(def b (reader/read-string ":_1"))
--- src/chordrecognizer/routes.clj
+++ src/chordrecognizer/routes.clj
@@ -4,10 +4,35 @@
         [hiccup.middleware :only (wrap-base-url)])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
-            [compojure.response :as response]))
+            [compojure.response :as response]
+            [overtone.music.pitch :as pitch]))
+
+(def invalid-request-body-error
+  {:type ::invalid-request-body
+   :message "The JSON or Clojure request body is invalid."})
+
+(defn read-body [body]
+  (let [data (try
+               (str (slurp body))
+               (catch java.lang.NullPointerException e (throw e)))]
+    (when (pos? (count data))
+      (try
+          (read-string data)
+          (catch java.lang.Exception _
+            (throw invalid-request-body-error))))))
+
+(defn make-safe-for-cljs-reader [k]
+  "Adds a prefix to the keyword. Handy for avoiding errors with keyword beginning with numbers."
+  (keyword (str "_" (.substring (str k) 1))))
+
+(defn recognize-chord [notes]
+  (if-let [chord (pitch/find-chord notes)]
+      (assoc chord :chord-type (make-safe-for-cljs-reader (:chord-type chord)))
+      "none"))
 
 (defroutes main-routes
   (GET "/" [] (index-page))
+  (POST "/chord" {body :body} (print-str (recognize-chord (read-body body))))
   (route/resources "/")
   (route/not-found "Page not found"))
 
