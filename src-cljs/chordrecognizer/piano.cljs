6d4c30eadf51ebc1b0db5731986a82d148a7771f
DanielT <turczanski@gmail.com>
2013-01-05 15:14:41 +0100

    cljs refactoring + fresh main.js prod build

--- src-cljs/chordrecognizer/piano.cljs
+++ src-cljs/chordrecognizer/piano.cljs
@@ -33,29 +33,30 @@
       (set-text! (by-id "chord") (str root " " type)))
     (set-text! (by-id "chord") (str "Chord not recognized"))))
 
-(defn key-clicked [e]
-  (let [key-div (.-target (.-evt e))
-        key (js/parseInt (attr key-div "value"))
-        class-set (set (classes key-div))]
-    (if (class-set "pressed")
-      (do
-        (set-classes! key-div (disj class-set "pressed"))
-        (swap! keys-pressed disj key))
-      (do 
-        (set-classes! key-div (conj class-set "pressed"))
-        (swap! keys-pressed conj key))
-      ))
-  (log (map #(str % ", ")  @keys-pressed))
+
+(defn get-chord-guess []
   (request "/chord" (fn [e]
                       (update-chord-guess nil)
                       (let [xhr (.-target e)
                             status (. xhr (getStatus))]
                         (if (= status 200)
                           (let [chord (reader/read-string
-                                             (. xhr (getResponseText)))]
+                                        (. xhr (getResponseText)))]
                             (update-chord-guess chord))))) 
            "POST" @keys-pressed))
-  ;(update-chord-guess (map #(str % ", ")  @keys-pressed)))
+
+(defn key-clicked [e]
+  (let [key-div (.-target (.-evt e))
+        key (js/parseInt (attr key-div "value"))
+        class-set (set (classes key-div))
+        alter-keys! (fn[action]
+                      (set-classes! key-div (action class-set "pressed"))
+                      (swap! keys-pressed action key))]
+    (if (class-set "pressed")
+      (alter-keys! disj)
+      (alter-keys! conj)))
+  ;(log (map #(str % ", ")  @keys-pressed))
+  (get-chord-guess))
 
 (defn ^:export initPiano[]
   (let [keys (.getElementsByClassName js/document "key")]
