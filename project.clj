14cf0a99ee69bfd2a6d81b2ec336e75e6da6edf5
DanielT <turczanski@gmail.com>
2013-01-05 13:56:34 +0100

    core ns fixes

--- project.clj
+++ project.clj
@@ -7,6 +7,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.8.0-RC14"]
                  [compojure "1.0.4"]
+                 [ring/ring-jetty-adapter "1.0.0-RC1" :exclusions [org.clojure/clojure
+                                                                   org.clojure/clojure-contrib]]
                  [hiccup "1.0.0"]
                  [hiccup-bootstrap "0.1.1"]
                  [org.clojure/google-closure-library "0.0-2029"]
--- src/chordrecognizer/core.clj
+++ src/chordrecognizer/core.clj
@@ -1,6 +1,7 @@
 (ns chordrecognizer.core
+  (:require [chordrecognizer.routes :as routes])
   (:use ring.adapter.jetty))
 
 (defn -main []
   (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
-    (run-jetty app {:port port})))
+    (run-jetty routes/app {:port port})))
